d00d80a18c732889f30da90ea30c4322a20c0f48
ryan.bollettino@gmail.com <bollo@bollo-laptop.localdomain>
2020-11-18 23:30:56 -0600

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
 # Add  code here!
+##n is a multiple of any integer between 2 and {\sqrt {n}}.
+def prime?(n)
+    return false if n < 2 
+    
+    array = (2..n-1).to_a
+
+    array.none? {|x| n % x == 0}
+
+end
+
+
+    
+#     array = (2..n-1).to_a
+
+#     array.each do |x|
+#    if n % x == 0
+#     return false
+#    end
+# #    elsif n % x != 0
+# #     return true
+# end
+# end
